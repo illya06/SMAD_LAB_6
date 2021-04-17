d46c698787a06e3167c8f14c524cd2e5d87d01bc
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-17 16:11:50 +0300

    global variables

---
+++ js/globals.js
@@ -0,0 +1,19 @@
+//input data
+var data_1 = []
+var data_2 = []
+
+//Laplasses coefitient
+var t = 0
+
+//midstat deviation
+var Sx = 0
+var Sy = 0
+
+//static corelation moment (KOVARIATION)
+var K = 0
+
+//coefitient of corelation
+var R = 0
+
+//Fishers coefitient
+var Z0 = 0
